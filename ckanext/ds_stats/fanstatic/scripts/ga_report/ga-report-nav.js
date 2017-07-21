5e2a9bed9ac79d852043920e332b69dc531fd791
Artem Bazykin <artem.bazykin@linkdigital.com.au>
2017-07-21 15:55:59 +0300

    DATASHADES-65 / Current state

--- ckanext/ds_stats/fanstatic/scripts/ga_report/ga-report-nav.js
+++ ckanext/ds_stats/fanstatic/scripts/ga_report/ga-report-nav.js
@@ -7,10 +7,10 @@
  * prefix - The prefix used on the ids.
  *
  */
-this.ckan.module('stats-nav', {
+this.ckan.module('ga-report-nav', {
   /* An options object */
   options: {
-    prefix: 'stats-'
+    prefix: 'ga-report-'
   },
 
   /* Initializes the module and sets up event listeners.
@@ -18,6 +18,7 @@ this.ckan.module('stats-nav', {
    * Returns nothing.
    */
   initialize: function () {
+      console.log('!!!!!!!!!!!!!!!!!!!');
     var location = this.sandbox.location;
     var prefix = this.options.prefix;
     var hash = location.hash.slice(1);
@@ -25,14 +26,14 @@ this.ckan.module('stats-nav', {
 
     // Update the hash fragment when the tab changes.
     this.el.on('shown', function (event) {
-        location.hash = event.target.hash.slice(prefix.length + 1);
-        var url = event.target.hash;
-        var menu_name = $('[href^=' + event.target.hash + ']').text();
-        var new_menu = $('<li class="breadcrumb-stats"><a href="' + url + '">' + menu_name + '</a></li>');
-        if ($('.breadcrumb').children()[2]) {
-            $('.breadcrumb').children()[2].remove();
-        }
-        $('.breadcrumb').children()[1].after(new_menu[0]);
+      location.hash = event.target.hash.slice(prefix.length + 1);
+      var url = event.target.hash;
+      var menu_name = $('[href^=' + event.target.hash + ']').text();
+      var new_menu = $('<li class="breadcrumb-ga-report"><a href="' + url + '">' + menu_name + '</a></li>');
+      if ($('.breadcrumb').children()[2]) {
+          $('.breadcrumb').children()[2].remove();
+      }
+      $('.breadcrumb').children()[1].after(new_menu[0]);
     });
 
     // Show the current tab if the location provides one.
